429cdfa0f498823c6337ba1baa6084ba401ada1e
ggeerraarrdd <pasetes@gmail.com>
2025-02-03 21:01:37 -0600

    Update 001 and 002 as test

--- test.py
+++ test.py
@@ -44,15 +44,44 @@ def process_nb(filename, nb):
         file.writelines(lines)
 
 
+
+
+def process_url(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        
+    url_old_1 = "Go to [Table of Contents](/README.md#contents)"
+    url_old_2 = "Go to [Overview](/README.md)"
+
+    for i in range(len(lines)):
+
+        if lines[i].strip().startswith(url_old_1):
+            lines[i] = "Go to [Index](../?tab=readme-ov-file#index)\\\n"
+    
+        if lines[i].strip().startswith(url_old_2):
+            lines[i] = "Go to [Overview](../?tab=readme-ov-file)\n"
+
+
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+
+
+
+
+
+
 # Get all files in solutions directory
 files = sorted(os.listdir('solutions/'))
 
 # Process files 1 through 5 # len(files) + 1
-for i in range(1, 128):
+for i in range(1, 3):
     filename = f'solutions/{files[i-1]}'
     
-    nb = "## NB\n\nTBD"
-    process_nb(filename, nb)
+    process_url(filename)
+
+    # nb = "## NB\n\nTBD"
+    # process_nb(filename, nb)
 
     #  process_submitted_solutions(filename)
 
